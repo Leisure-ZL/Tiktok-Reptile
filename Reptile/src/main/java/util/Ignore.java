6f1ead8efd6e5ad76c97cc110ad651a0d20915d7
Leisure-ZL <1736740364@qq.com>
2022-04-06 20:04:31 +0800

    4/6

--- Reptile/src/main/java/util/Ignore.java
+++
@@ -1,9 +0,0 @@
-package util;
-
-public class Ignore {
-    static final String jdbcUrl = "jdbc:mysql://119.3.77.254:3306/reptile?useSSL=false&serverTimezone=UTC";
-    static final String jdbcUser = "root";
-    static final String jdbcPass = "ZLzhouliang0.";
-
-
-}
